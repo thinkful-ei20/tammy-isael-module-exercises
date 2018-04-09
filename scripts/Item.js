4e6c49ce741853158957345e703b19697beb30be
Tammy Tian <tiantammy@outlook.com>
2018-04-10 00:45:44 +0800

    added Item namespace

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,4 +1,22 @@
 'use strict';
 const Item = (function(){
+
+  const validateName = function(name){
+    if (name === ''){
+      throw 'Name does not exist.';
+    }
+  };
+
+  const create = function(name){
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    };
+  };
   const foo = 'bar';
-});
+  return {
+    validateName,
+    create,
+  };
+}());
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,22 @@
-/* global shoppingList, cuid, store */
+/* global shoppingList, cuid, store, Item */
 'use strict';
 // eslint-disable-next-line no-unused-vars
 
 $(document).ready(function() {
   //console.log(store);
   //console.log(foo);
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  console.log(itemNames);
+  shoppingList.render();
+
   shoppingList.bindEventListeners();
   shoppingList.render();
   console.log(Item);
